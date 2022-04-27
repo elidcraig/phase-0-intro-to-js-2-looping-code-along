e6fc53c75c9fce876210d7fe7cb1edbc988e300e
Eli Craig <elidcraig@gmail.com>
2022-04-26 22:30:12 -0700

    commit tree 0ef0d214a6479e271829b1abdd43db8ed6d073c4

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+function writeCards(names, event) {
+    const cards = []
+    for (let i = 0; i < names.length; i++) {
+        cards.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+    }
+    return cards 
+}
+
+function countDown() {
+    let i = 10
+    while (i >= 0) {
+        console.log(i)
+        i--
+    }
+}
